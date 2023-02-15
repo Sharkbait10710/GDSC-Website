3cf9dead6a348239fc424bfa14936f6221be0b25
Sharkbait10710 <Dang.Le10710@gmail.com>
2023-02-15 09:27:19 -0800

    [Frontend] Finished rough draft of sign in

--- app/src/components/Home/Navbar.js
+++ app/src/components/Home/Navbar.js
@@ -1,9 +1,12 @@
 // Node imports
 import * as React                   from "react"
 import { motion, AnimatePresence }  from "framer-motion"
-
+import TextField                    from '@mui/material/TextField';
+import Button                       from '@mui/material/Button';
 // MUI Icons
 import MenuIcon                     from '@mui/icons-material/Menu';
+import LoginIcon                    from '@mui/icons-material/Login';
+import SendTimeExtensionIcon        from '@mui/icons-material/SendTimeExtension';
 
 // Image imports
 import GDSC_logo                    from "../../imgs/GDSC_Logo.png"
@@ -34,6 +37,29 @@ const Navbar = (props) => {
     
     const [showOptions, setshowOptions] = React.useState(() => {
         return false
+    })
+
+    const [signIn, setsignIn] = React.useState(() => {
+        return false
+    })
+
+    const [register, setRegister] = React.useState(() => {
+        return false
+    })
+
+    React.useEffect(() => {
+        const handleKeypresses = (event) => {
+            if (event.key==`Escape`) {
+                setRegister(false);
+                setsignIn(false)
+            }
+        }
+
+        document.addEventListener('keydown', handleKeypresses);
+
+        return function cleanupListener() {
+            document.removeEventListener('keydown', handleKeypresses)
+        }
     })
 
     return (
@@ -117,7 +143,7 @@ const Navbar = (props) => {
                     transition="transition"
                     className="navbar-link"
                     style={{
-                        marginRight: "24%",
+                        marginRight: "32%",
                         fontFamily: "Google Sans",
                         position: "absolute",
                         top: "2%"
@@ -135,7 +161,7 @@ const Navbar = (props) => {
                     transition="transition"
                     className="navbar-link"
                     style={{
-                        marginRight: "16%",
+                        marginRight: "24%",
                         fontFamily: "Google Sans",
                         position: "absolute",
                         top: "2%"
@@ -153,7 +179,7 @@ const Navbar = (props) => {
                     transition="transition"
                     className="navbar-link"
                     style={{
-                        marginRight: "8%",
+                        marginRight: "16%",
                         fontFamily: "Google Sans",
                         position: "absolute",
                         top: "2%"
@@ -171,11 +197,28 @@ const Navbar = (props) => {
                     transition="transition"
                     className="navbar-link"
                     style={{
-                        marginRight: "0%",
+                        marginRight: "8%",
                         fontFamily: "Google Sans",
                         position: "absolute",
                         top: "2%"
                     }}><a onClick={props.setProject}>Projects</a>
+                </motion.div>}
+                {props.sizeWidth == 3 && <motion.div
+                    variants={linkVariant}
+                    whileHover={{
+                        scale: 1.2,
+                        borderBottom: "solid #e84438"
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                    transition="transition"
+                    className="navbar-link"
+                    style={{
+                        marginRight: "0%",
+                        fontFamily: "Google Sans",
+                        position: "absolute",
+                        top: "2%"
+                    }}><a onClick={() => setsignIn(true)}>Sign In</a>
                 </motion.div>}
                 <AnimatePresence>
                     {showOptions && props.sizeWidth != 3 &&
@@ -227,6 +270,195 @@ const Navbar = (props) => {
                                 <div>Meetup</div>
                     </motion.div>}
                 </AnimatePresence>
+                {signIn && <AnimatePresence>
+                    <motion.div
+                        initial={{
+                            y: "-100vh",
+                            opacity: 0
+                        }}
+                        animate={{
+                            y: "0vh",
+                            opacity: 1
+                        }}
+                        transition={{
+                            duration: 0.3
+                        }}
+                        style={{
+                            position: "absolute",
+                            top: "10%",
+                            right: "10%",
+
+                            width: "20vw",
+                            height: "23vh",
+                            border: "1px solid",
+                            backgroundColor: "white",
+                            borderRadius: "5px",
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "space-evenly",
+
+                            zIndex: 2
+                        }}>
+                        <motion.div
+                            initial={{
+                                scale: 1.1
+                            }}
+                            animate={{
+                                scale: 1.1
+                            }}
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+
+                                width: "70%",
+                                height: "80%",
+                                margin: "30px"
+                            }}>
+                            <TextField
+                                id="standard-password-input"
+                                label="Username"
+                                type="username"
+                                autoComplete="current-password"
+                                variant="standard"
+                                />
+                            <TextField
+                                id="standard-password-input"
+                                label="Password"
+                                type="password"
+                                autoComplete="current-password"
+                                variant="standard"
+                                />
+                        </motion.div>
+                        <motion.div
+                            style={{display: "flex",
+                                justifyContent: "space-evenly",
+
+                                height: "40%",
+                                marginBottom: "20px",
+                                marginRight: "100px"
+                            }}>
+                            <Button variant="contained" startIcon={<LoginIcon />}>
+                                Login
+                            </Button>
+                            <Button variant="outlined" onClick={() => setRegister(true)}>
+                                Register
+                            </Button>
+                        </motion.div>
+                    </motion.div>    
+                </AnimatePresence>}
+                {register && <AnimatePresence>
+                    <motion.div
+                        initial={{
+                            opacity: 0,
+                            y: "100vh"
+                        }}
+                        animate={{
+                            opacity: 1,
+                            y: "0vh"
+                        }}
+                        transition={{
+                            duration: 0.5
+                        }}
+                        exit={{
+                            opacity: 0,
+                            transition: {
+                                duration: 1
+                            }
+                        }}
+                        style={{
+                            position: "absolute",
+                            width: "20vw",
+                            height: "80vh",
+                            top: "10vh",
+                            right: "34vw",
+
+                            backgroundColor: "white",
+
+                            border: "1px solid",
+                            borderRadius: "10px",
+                            boxShadow: "5px 5px #d9d9d9",
+
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "space-evenly",
+                            marginLeft: "6%",
+                            zIndex: 3,
+                            backgroundColor: "white"
+                        }}>
+                        <motion.div
+                            initial={{
+                                scale: 2
+                            }}
+                            animate={{
+                                scale: 2
+                            }}
+                            style={{
+                                height: "10vh",
+                                marginLeft: "37%"
+                            }}>
+                            <TextField
+                                required
+                                id="standard-required"
+                                label="Username"
+                                variant="standard"
+                                size="normal"
+                                />
+                        </motion.div>
+                        <motion.div
+                            initial={{
+                                scale: 2
+                            }}
+                            animate={{
+                                scale: 2
+                            }}
+                            style={{
+                                height: "10vh",
+                                marginLeft: "37%"
+                            }}>
+                            <TextField
+                                required
+                                id="standard-required"
+                                label="Name"
+                                variant="standard"
+                                size="normal"
+                                />
+                        </motion.div>
+                        <motion.div
+                            initial={{
+                                scale: 2
+                            }}
+                            animate={{
+                                scale: 2
+                            }}
+                            style={{
+                                height: "10vh",
+                                marginLeft: "37%"
+                            }}>
+                            <TextField
+                                required
+                                id="standard-required"
+                                label="Permission Code"
+                                variant="standard"
+                                size="normal"
+                                />
+                        </motion.div>
+                        <motion.div
+                            initial={{
+                                scale: 2
+                            }}
+                            animate={{
+                                scale: 2
+                            }}
+                            style={{
+                                height: "10vh",
+                                marginLeft: "47%"
+                            }}>
+                        <Button variant="contained" startIcon={<SendTimeExtensionIcon />}>
+                                Request
+                            </Button>
+                        </motion.div>
+                    </motion.div>
+                </AnimatePresence>}
         </div>
     );
 }
