b46836fd2adf80cb0b0961264c23d730a22cf40a
william-b-henderson <william_henderson@berkeley.edu>
2023-02-16 01:01:36 -0800

    Refactor entire website, remove redundant code and fix errors, made it prettier

--- app/src/firebase/Auth.jsx
+++ app/src/firebase/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
@@ -9,18 +9,18 @@ import { Button } from '@mui/material';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA_vpnFneHF9Dwa9PNIOBtaKihK6wz5aVM",
-    authDomain: "gdsc-website-9d5c2.firebaseapp.com",
-    projectId: "gdsc-website-9d5c2",
-    storageBucket: "gdsc-website-9d5c2.appspot.com",
-    messagingSenderId: "284181410073",
-    appId: "1:284181410073:web:77caf34f069a478183d588",
-    measurementId: "G-B9MSK6D2ML"
-  };
+  apiKey: 'AIzaSyA_vpnFneHF9Dwa9PNIOBtaKihK6wz5aVM',
+  authDomain: 'gdsc-website-9d5c2.firebaseapp.com',
+  projectId: 'gdsc-website-9d5c2',
+  storageBucket: 'gdsc-website-9d5c2.appspot.com',
+  messagingSenderId: '284181410073',
+  appId: '1:284181410073:web:77caf34f069a478183d588',
+  measurementId: 'G-B9MSK6D2ML',
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+getAnalytics(app);
 
 // Signs-in Friendly Chat.
 export async function signIn() {
@@ -55,51 +55,6 @@ export function addAuthListener(listenerFunction) {
 }
 
 export default function Auth() {
-  // Initialize firebase auth
-  function initFirebaseAuth() {
-    // Listen to auth state changes.
-    onAuthStateChanged(getAuth(), authStateObserver);
-  }
-
-  // Triggers when the auth state change for instance when the user signs-in or signs-out.
-  function authStateObserver(user) {
-    if (user) {
-      // User is signed in!
-      // Get the signed-in user's profile pic and name.
-      setProfilePicUrl(getProfilePicUrl());
-      setUserName(getUserName());
-
-      // Set the user's profile pic and name.
-      //   userPicElement.style.backgroundImage =
-      //     'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
-      //   userNameElement.textContent = userName;
-
-      //   // Show user's profile and sign-out button.
-      //   userNameElement.removeAttribute('hidden');
-      //   userPicElement.removeAttribute('hidden');
-      //   signOutButtonElement.removeAttribute('hidden');
-
-      //   // Hide sign-in button.
-      //   signInButtonElement.setAttribute('hidden', 'true');
-
-      //   // We save the Firebase Messaging Device token and enable notifications.
-      //   saveMessagingDeviceToken();
-      // } else {
-      //   // User is signed out!
-      //   // Hide user's profile and sign-out button.
-      //   userNameElement.setAttribute('hidden', 'true');
-      //   userPicElement.setAttribute('hidden', 'true');
-      //   signOutButtonElement.setAttribute('hidden', 'true');
-
-      //   // Show sign-in button.
-      //   signInButtonElement.removeAttribute('hidden');
-    }
-  }
-  initFirebaseAuth();
-
-  const [userName, setUserName] = useState('');
-  const [profilePicUrl, setProfilePicUrl] = useState('');
-
   return (
     <div>
       <Button variant="contained" id="signin" onClick={signIn}>
