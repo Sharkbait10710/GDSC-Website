bb31e39bf18e9daf05447b61bad0c29654e49a39
Sharkbait10710 <Dang.Le10710@gmail.com>
2023-02-21 01:57:21 -0800

    [Profile] Made it look better

--- app/src/pages/Profile.jsx
+++ app/src/pages/Profile.jsx
@@ -3,6 +3,8 @@ import { Typography } from '@mui/material';
 import { addAuthListener, getProfilePicUrl, getUserName, isUserSignedIn } from '../firebase/Auth';
 import { getUserProfile } from '../firebase/Firestore';
 import { getAuth } from 'firebase/auth';
+// Image imports
+import profile_bg from '../imgs/profile_bg.png';
 
 const Profile = (props) => {
   const [userName, setUserName] = useState('');
@@ -55,29 +57,92 @@ const Profile = (props) => {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-evenly',
-        top: '10%',
         height: '80vh',
-        width: '80%',
+        width: '80%'
       }}
     >
       {isSignedIn ? (
         <>
-          {profilePicUrl !== '' && <img alt="profile" src={profilePicUrl} />}
-          <Typography>Welcome Home, {userName}</Typography>
-          {profile && (
-            <ul>
-              {Object.entries(profile).map(([key, value], index) => {
-                if (key === 'timestamp') {
-                } else {
-                  return (
-                    <li key={index}>
-                      {key} - {value.toString()}
-                    </li>
-                  );
-                }
-              })}
-            </ul>
-          )}
+            <div
+                style={{
+                    position: "absolute",
+                    height: '80vh',
+                    width: '80%',
+                    overflow: "hidden",
+                    border: "1px solid #ececec",
+                    boxShadow: "8px 10px #d9d9d9",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    zIndex: -1
+                }}>
+                    <img src={profile_bg} alt="bg" height={1500} />
+            </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              height: '70vh',
+              width: '80vw',
+            }}
+          >
+            {profilePicUrl !== '' && (
+              <div
+                style={{
+                  height: '20%',
+                  width: '90%',
+                  padding: '30px',
+                  display: 'flex',
+                  flexDirection: 'row-reverse',
+                  alignItems: 'center',
+                }}
+              >
+                <div
+                  style={{
+                    border: '1px solid',
+                    borderRadius: '90px',
+                    overflow: 'hidden',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <img alt="profile" src={profilePicUrl} height="150px" />
+                </div>
+              </div>
+            )}
+            <Typography
+              sx={{
+                fontFamily: 'Google Sans',
+                marginLeft: '50px',
+                fontSize: '60px',
+              }}
+            >
+              Welcome Home, {userName}
+            </Typography>
+            {profile && (
+              <ul>
+                {Object.entries(profile).map(([key, value], index) => {
+                  if (key === 'timestamp') {
+                  } else {
+                    return (
+                      <li key={index} className="profileEle">
+                        <div
+                          style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                          }}
+                        >
+                          <div style={{ fontWeight: '600', marginRight: '5px', fontSize: '30px' }}>{key}</div>{' '}
+                          <div style={{marginTop: "5px"}}>- {value.toString()} </div>
+                        </div>
+                      </li>
+                    );
+                  }
+                })}
+              </ul>
+            )}
+          </div>
           {/* <Typography>{profile}</Typography> */}
         </>
       ) : (
